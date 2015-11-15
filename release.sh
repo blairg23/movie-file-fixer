39317b539cbcb604b278c4d97982a187a7265c2e
Blair Gemmer <blair.gemmer@gmail.com>
2015-11-15 03:56:19 -0700

    testing automatic release shell script

---
+++ release.sh
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "$1"
+git push && git push --mirror https://github.com/blairg23/movie-file-fixer
---
+++ release_win.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m %*
+git push && git push --mirror https://github.com/blairg23/movie-file-fixer
