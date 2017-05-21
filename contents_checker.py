1a5cec30e31cea0a9317299d1e17d37af1a54f28
blair gemmer <blairg23@gmail.com>
2017-05-21 06:03:37 -0600

    Updated contents file checker script to support Python3

--- contents_checker.py
+++ contents_checker.py
@@ -13,15 +13,15 @@ Checks the contents.json for any possible errors.
 import os
 import json
 
-contents_file_directory = 'J:\\to_sort'
+contents_file_directory = 'H:\\Films'
 contents_filename = 'contents.json'
 contents_fullpath = os.path.join(contents_file_directory, contents_filename)
 
-films_directory = 'J:\Films'
+films_directory = 'H:\Films'
 films_list = os.listdir(films_directory)
 film_titles = []
 
-with open(contents_fullpath) as infile:
+with open(contents_fullpath, encoding='utf-8') as infile:
 	indexed_titles = json.load(infile)
 
 
@@ -30,9 +30,9 @@ for indexed_title in indexed_titles['Titles']:
 	film_title = indexed_title['title']
 	film_titles.append(film_title)
 	if film_title not in films_list:
-		print film_title
+		print(film_title)
 
 # Second, check our film directory against our contents file:
 for film_title in films_list:
 	if film_title not in film_titles:
-		print film_title
+		print(film_title)
