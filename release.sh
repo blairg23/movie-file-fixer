a7d4acd4d59d676609f484fcab19e390f9a3b75f
blairg23 <blairg23@gmail.com>
2018-11-09 12:43:31 -0700

    Removed old shell scripts

--- release.sh
+++
@@ -1,4 +0,0 @@
-git add --all
-git commit -m "$1"
-git push && git push --mirror https://github.com/blairg23/movie-file-fixer
-git status
--- release_win.bat
+++
@@ -1,4 +0,0 @@
-git add --all
-git commit -m %*
-git push && git push --mirror https://github.com/blairg23/movie-file-fixer
-git status
